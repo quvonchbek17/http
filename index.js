79655d8069cc4654d7ed11855718dd5b6c70c5f0
Jahongir <akhrasmiy@gmail.com>
2023-04-27 10:43:39 +0500

    fghjkl;

--- index.js
+++ index.js
@@ -4,7 +4,18 @@ const server = http.createServer((req, res) => {
     let url = req.url
     if(url == "/"){
         res.end("ok")
+        
     }
+
+    if(url == "/users"){
+        res.end("users")
+    }
+
+    if(url == "/comments"){
+        res.end("users")
+    }
+    
+
 })
 
 
